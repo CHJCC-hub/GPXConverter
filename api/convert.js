99cfea67b382eefd44a0160e81154ceb63ef5699
CHJCC-hub <chjcc31628@gmail.com>
2026-04-16 09:45:39 +0800

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -177,9 +177,10 @@ if (coord) {
         finalPoints.forEach(p => {
             gpx += `<wpt lat="${p.lat}" lon="${p.lon}">\n`;
 
-			if (p.name && p.name.trim() !== "") {
-				let safeName = p.name.replace(/]]>/g, "]]]]><![CDATA[>");
-				gpx += `<name><![CDATA[${safeName}]]></name>\n`;
+			 if (p.name) {
+            let safeName = p.name.replace(/]]>/g, "]]]]><![CDATA[>");
+            gpx += `    <name><![CDATA[${safeName}]]></name>
+				
 			}
 
             gpx += `</wpt>\n`;
